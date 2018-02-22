812e2cfc6e18c1b29b949d44b0567e51a35446cb
Megan McEwan <meganmcewan@Megans-MacBook-Pro.local>
2018-02-22 13:21:59 -0500

    register merchant added

--- src/requests.js
+++ src/requests.js
@@ -10,23 +10,81 @@ var config = {
     storageBucket: "washr-data.appspot.com",
     messagingSenderId: "700840172175"
   };
- const app = firebase.initializeApp(config);
-
-
+const app = firebase.initializeApp(config);
 
 var database = firebase.database();
 
 
-export function signup(email, username, password) {
+
+/////////login, sign up and singout functions//////////
+
+export function registerMerchant (merchantObj, priceObj){
+    // this is to write the data
+    var userID = merchantObj.userID
+
+    database.ref('/Merchants/' + userID).set({ 
+      userID: userID,
+      businessAddress: merchantObj.businessAddress,
+      city: merchantObj.city,
+      province: merchantObj.province,
+      postalCode: merchantObj.postalCode,
+      businessPhoneNum: merchantObj.businessPhoneNum
+  
+    })
+
+    console.log('merchant is registered')
+
+    database.ref('/Prices/'+ userID + '/Regular/').set({
+      userID: userID,
+      trousers: priceObj.regular.trousers,
+      suit: priceObj.regular.suit,
+      overcoat: priceObj.regular.overcoat,
+      ladiesSuit: priceObj.regular.ladiesSuit,
+      dress: priceObj.regular.dress,
+      skirt: priceObj.regular.skirt,
+      jacket: priceObj.regular.jacket,
+      blouse: priceObj.regular.blouse,
+      tie: priceObj.regular.tie
+
+    })
+
+    console.log("regular prices are registered for user:", userID)
+
+    database.ref('/Prices/'+ userID + '/Express/').set({
+      userID: userID,
+      trousers: priceObj.express.trousers,
+      suit: priceObj.express.suit,
+      overcoat: priceObj.express.overcoat,
+      ladiesSuit: priceObj.express.ladiesSuit,
+      dress: priceObj.express.dress,
+      skirt: priceObj.express.skirt,
+      jacket: priceObj.express.jacket,
+      blouse: priceObj.express.blouse,
+      tie: priceObj.express.tie
+
+    })
+
+    console.log('express prices are registered for user:', userID)
+  
+}
+
+export function registerPrices (priceObj){
+
+}
+
+
+export function registerUser(email, userType, password ) {
   return firebase.auth().createUserWithEmailAndPassword(email, password)
   .then(firebaseUser => {
     database.ref('/Users/' + firebaseUser.uid).set({
-        name: username,
-        email: email
-         
+        email: email,
+        userType: userType.toLowerCase(),
+      
     });
-    return firebaseUser;
+    console.log(firebaseUser.uid)
+    return firebaseUser.uid;
   })
+
   .catch(function(error) {
     // Handle Errors here.
     var errorCode = error.code;
@@ -39,6 +97,8 @@ export function signup(email, username, password) {
     console.log(error);
   });
 }
+
+
 
 export function login (email, password){
     return firebase.auth().signInWithEmailAndPassword(email, password)
@@ -73,9 +133,13 @@ export function signout(){
   });
 }
 
+
+///////// order related functions ///////
+
+
 export function addOrder(orderID, userID, info1, info2){
 
-    userID = database.
+  // this is to write the data
 
   database.ref('/Orders/' + orderID).set({
     orderID: orderID,
@@ -85,6 +149,13 @@ export function addOrder(orderID, userID, info1, info2){
     orderConf: Math.floor((Math.random() * 100000) + 1)
 
   })
+
+  database.ref('Merchants/' +userID + '/Orders/').push().set({
+    allOrders: orderID
+  })
+
+  //this is to read the data 
+
   database.ref('/Orders/'+ orderID).once('value')
   .then(snapshot => console.log(snapshot.val().orderConf))
 
@@ -92,6 +163,9 @@ export function addOrder(orderID, userID, info1, info2){
   // .then (showAlert => alert(info))
 
 }
+
+
+///////  testing area ////////
 
 
 
